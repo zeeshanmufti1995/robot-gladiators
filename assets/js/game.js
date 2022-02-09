b65c416898832b83d45983b1f5893a754bf95546
zeeshan <mycomputer@Zeeshans-MacBook-Pro.local>
2022-02-09 07:09:35 -0600

    function and prompt

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+//   fight();
